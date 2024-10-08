a3018a75771735fd1403412d30888a09e6cae22f
gplorovg <gplorovg@gmail.com>
2024-10-09 01:17:13 +0300

    complete aes256_gcm

--- aes256_gcm.py
+++ aes256_gcm.py
@@ -14,12 +14,22 @@ class Aes256GSM:
     @staticmethod
     def decrypt(key: bytes| bytearray | memoryview, data: any, nonce: bytes| bytearray | memoryview, tag: bytes) \
             -> tuple[bytes, str]:
-        cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
+        try:
+            if type(key) is str:
+                raise TypeError("Incorrect AES key type (str). Expected: bytes| bytearray | memoryview")
+
+            cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
+
+        except ValueError as e:
+            return bytes(), f"Cipher creation failed: {str(e)}"
+        except TypeError as e:
+            return bytes(), f"Cipher creation failed due to wrong data type: {str(e)}"
 
         try:
             decrypted_data = cipher.decrypt_and_verify(data, tag)
-        except Exception:
-            return bytes(), "Error"
+        except ValueError as e:
+            return bytes(), f"Decryption failed: {str(e)}"
+        except TypeError as e:
+            return bytes(), f"Decryption failed due to wrong data type: {str(e)}"
 
-        return decrypted_data, "OK"
-
+        return decrypted_data, ""
