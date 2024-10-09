b9047e93fb55e7172fc58387801c4c4c125908b4
gplorovg <gplorovg@gmail.com>
2024-10-09 19:38:06 +0300

    failed to implement 4 part secret split

--- shamir.py
+++ shamir.py
@@ -1,10 +1,11 @@
 import random
+import json
 import secrets
 import multiprocessing
-
+from functools import partial
 
 class Shamir256:
-    def __init__(self, divisor: int = 2 ** 64, high_value: int = 2 ** 128):
+    def __init__(self, divisor: int = 2 ** 64, high_value: int = 2 ** 65):
         self.divisor = divisor
         self.high_value = high_value
 
@@ -23,7 +24,14 @@ class Shamir256:
             result = result * x + coeff
         return result % self.divisor
 
-    def __count_sub_secret(self, secret: int, threshold: int, num_shares: int) -> list[tuple[int, int]]:
+    def __add_paddings(self, s: bytes) -> bytes:
+        if len(s) < 150:
+            return s + bytes(0) * (150 - len(s))
+        return s
+
+    def __count_sub_secret(self, part: tuple[int, int], threshold: int, num_shares: int)\
+            -> dict[str: int, str: list[tuple[int, int]]]:
+        secret, secret_id = part
         coeffs = [secret] + self.__generate_coefficients(threshold)
         shares = list()
 
@@ -31,16 +39,34 @@ class Shamir256:
             x = random.randint(1, self.high_value)
             y = self.__evaluate_polynomial_horner_scheme(x, coeffs)
             shares.append((x, y))
+        response = {
+            "id": secret_id,
+            "shares": shares,
+        }
+        return response
 
-        return shares
+    @staticmethod
+    def __part_shares_to_bytes(secret_parts_shares: list[dict[str: int, str: list[tuple[int, int]]]])\
+            -> list[tuple[int, bytes]]:
+        shares = [s["shares"] for s in sorted(secret_parts_shares, key=lambda d: d["id"])]
+        secret_shares = list(enumerate(map(json.dumps, list(zip(*shares)))))
+
 
     def split_secret(self, secret: bytes, threshold: int, num_shares: int) -> list[tuple[int, bytes]]:
         if len(secret) != 32:
             raise ValueError(f"Unacceptable secret size ({len(secret)}). Expected 32 bytes")
         secret_parts = self.__bytes_to_ints(secret)
+        secret_count_with_param = partial(self.__count_sub_secret, threshold=threshold, num_shares=num_shares)
+        secret_shares = list()
 
         with multiprocessing.Pool(processes=4) as pool:
-            secret_parts_shares = pool.map(self.__count_sub_secret, secret_parts)
+            secret_parts_shares = pool.map(secret_count_with_param, secret_parts)
+
+        for part_shares in secret_parts_shares:
+
+            json.dumps()
+
+        return secret_shares
 """
 - pool.map() - как добавить дополнительные параметры
 - мастер-секрет разбивается на 4 подсекрета
