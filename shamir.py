ce8ec0e0552492677375d97ebf97d9cf390f0d22
gplorovg <gplorovg@gmail.com>
2024-10-09 03:15:11 +0300

    add uncompleted split_secret & some todos

--- shamir.py
+++ shamir.py
@@ -34,38 +34,26 @@ class Shamir256:
 
         return shares
 
+    def split_secret(self, secret: bytes, threshold: int, num_shares: int) -> list[tuple[int, bytes]]:
+        if len(secret) != 32:
+            raise ValueError(f"Unacceptable secret size ({len(secret)}). Expected 32 bytes")
+        secret_parts = self.__bytes_to_ints(secret)
 
-def recover_secret(shares, p):
-    """Восстановление секрета с использованием интерполяции Лагранжа"""
-    x_s, y_s = zip(*shares)
-    return lagrange_interpolation(0, x_s, y_s, p)
-
-
-# Параметры схемы Шамира
-k = 3  # Минимальное количество частей для восстановления секрета
-n = 5  # Общее количество частей
-p = 2 ** 257 - 93  # Простое число, которое больше, чем 256-битное значение
-
-# Генерация 32-байтного ключа (256 бит)
-secret_key = int.from_bytes(os.urandom(32), byteorder='big')
-
-print(f"Секрет (ключ): {secret_key}")
-
-# Разделяем секрет на n частей с минимальным порогом k
-shares = split_secret(secret_key, k, n, p)
-
-print("\nЧасти секрета:")
-for i, share in enumerate(shares):
-    print(f"Часть {i + 1}: (x={share[0]}, y={share[1]})")
-
-# Восстановление секрета из первых k частей
-selected_shares = shares[:k]
-recovered_key = recover_secret(selected_shares, p)
-
-print(f"\nВосстановленный секрет (ключ): {recovered_key}")
-
-# Проверяем, совпадает ли восстановленный секрет с исходным
-if recovered_key == secret_key:
-    print("Секрет успешно восстановлен!")
-else:
-    print("Ошибка при восстановлении секрета!")
+        with multiprocessing.Pool(processes=4) as pool:
+            secret_parts_shares = pool.map(self.__count_sub_secret, secret_parts)
+"""
+- pool.map() - как добавить дополнительные параметры
+- мастер-секрет разбивается на 4 подсекрета
+- каждый из подсекретов разбивается на части (кол-вом num_shares)
+- от каждого подсекрета берётся одна часть в кучку
+- получится num_shares кучек по 4 части в каждой
+- для каждой кучки провести шифрование, чтобы потом можно было извлечь каждую часть и
+ определить к какому подсекрету она относится
+- зашифрованные кучки и есть части мастер-ключа, которые раздаются админам
+- при объединении своих частей мастер-ключа,
+ каждая из частей расшифровывается в кучку из 4х точек от разных подсекретов
+- если частей достаточно, то эти кучки разбиваются по подсекретам
+- новые разбивки по точкам каждого подсекрета объединяются в подсекрет
+- подсекреты, соблюдая порядок, объединяются в секрет
+- секрет возвращается в формате байт
+"""
