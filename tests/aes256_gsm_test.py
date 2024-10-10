b041c65f45f3e1adbe1a46b6a2e92f6c8204d2b7
gplorovg <gplorovg@gmail.com>
2024-10-10 03:30:49 +0300

    improve aes256 tests by moving frequently used data to ciphertext_nonce_tag

--- tests/aes256_gsm_test.py
+++ tests/aes256_gsm_test.py
@@ -14,55 +14,57 @@ def root_key():
 def master_key():
     return get_random_bytes(32)
 
-def test_encrypt_decrypt_master_key(cipher, root_key, master_key):
+@pytest.fixture
+def ciphertext_nonce_tag(cipher, root_key, master_key):
     ciphertext, nonce, tag = cipher.encrypt(root_key, master_key)
-    decrypted_master_key, _ = cipher.decrypt(root_key, ciphertext, nonce, tag)
+    return ciphertext, nonce, tag
+
+def test_encrypt_decrypt_master_key(cipher, root_key, ciphertext_nonce_tag):
+    decrypted_master_key, _ = cipher.decrypt(root_key, *ciphertext_nonce_tag)
     assert master_key == decrypted_master_key, "Расшифрованный мастер-ключ не совпадает с оригиналом"
 
-def test_decrypt_cipher_key_value_error(cipher, root_key, master_key):
-    ciphertext, nonce, tag = cipher.encrypt(root_key, master_key)
+def test_decrypt_cipher_key_value_error(cipher, root_key, ciphertext_nonce_tag):
     wrong_root_key = root_key[:-1]
-    decrypted_master_key, err = cipher.decrypt(wrong_root_key, ciphertext, nonce, tag)
+    decrypted_master_key, err = cipher.decrypt(wrong_root_key, *ciphertext_nonce_tag)
     assert "Cipher creation failed:" in err
 
-def test_decrypt_cipher_key_type_error(cipher, root_key, master_key):
-    ciphertext, nonce, tag = cipher.encrypt(root_key, master_key)
+def test_decrypt_cipher_key_type_error(cipher, root_key, ciphertext_nonce_tag):
     wrong_root_key = "123"
-    decrypted_master_key, err = cipher.decrypt(wrong_root_key, ciphertext, nonce, tag)
+    decrypted_master_key, err = cipher.decrypt(wrong_root_key, *ciphertext_nonce_tag)
     assert "Cipher creation failed due to wrong data type:" in err
 
-def test_decrypt_cipher_nones_type_error(cipher, root_key, master_key):
-    ciphertext, nonce, tag = cipher.encrypt(root_key, master_key)
+def test_decrypt_cipher_nones_type_error(cipher, root_key, ciphertext_nonce_tag):
+    ciphertext, nonce, tag = ciphertext_nonce_tag
     wrong_nonce = 123
     decrypted_master_key, err = cipher.decrypt(root_key, ciphertext, wrong_nonce, tag)
     assert "Cipher creation failed due to wrong data type:" in err
 
-def test_decrypt_cipher_nones_value_error(cipher, root_key, master_key):
-    ciphertext, nonce, tag = cipher.encrypt(root_key, master_key)
+def test_decrypt_cipher_nones_value_error(cipher, root_key, ciphertext_nonce_tag):
+    ciphertext, nonce, tag = ciphertext_nonce_tag
     wrong_nonce = nonce[:-5]
     decrypted_master_key, err = cipher.decrypt(root_key, ciphertext, wrong_nonce, tag)
     assert "Decryption failed: MAC check failed" == err
 
-def test_decrypt_data_value_error(cipher, root_key, master_key):
-    ciphertext, nonce, tag = cipher.encrypt(root_key, master_key)
+def test_decrypt_data_value_error(cipher, root_key, ciphertext_nonce_tag):
+    ciphertext, nonce, tag = ciphertext_nonce_tag
     wrong_data = ciphertext[:-2]
     decrypted_master_key, err = cipher.decrypt(root_key, wrong_data, nonce, tag)
     assert "Decryption failed: MAC check failed" == err
 
-def test_decrypt_data_type_error(cipher, root_key, master_key):
-    ciphertext, nonce, tag = cipher.encrypt(root_key, master_key)
+def test_decrypt_data_type_error(cipher, root_key, ciphertext_nonce_tag):
+    ciphertext, nonce, tag = ciphertext_nonce_tag
     wrong_data = 123
     decrypted_master_key, err = cipher.decrypt(root_key, wrong_data, nonce, tag)
     assert "Decryption failed due to wrong data type:" in err
 
-def test_decrypt_tag_type_error(cipher, root_key, master_key):
-    ciphertext, nonce, tag = cipher.encrypt(root_key, master_key)
+def test_decrypt_tag_type_error(cipher, root_key, ciphertext_nonce_tag):
+    ciphertext, nonce, tag = ciphertext_nonce_tag
     wrong_tag = str(tag)
     decrypted_master_key, err = cipher.decrypt(root_key, ciphertext, nonce, wrong_tag)
     assert "Decryption failed due to wrong data type:" in err
 
-def test_decrypt_tag_value_error(cipher, root_key, master_key):
-    ciphertext, nonce, tag = cipher.encrypt(root_key, master_key)
+def test_decrypt_tag_value_error(cipher, root_key, ciphertext_nonce_tag):
+    ciphertext, nonce, tag = ciphertext_nonce_tag
     wrong_tag = tag[:-2]
     decrypted_master_key, err = cipher.decrypt(root_key, ciphertext, nonce, wrong_tag)
     assert "Decryption failed: MAC check failed" in err
